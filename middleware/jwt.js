f6bec4d5b18ee7ad4befcfd9bf68bdde512b8675
ASUS <you@example.com>
2025-04-17 23:45:26 +0200

    update code

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -2,17 +2,21 @@ import jwt from "jsonwebtoken";
 import createError from "../utils/createError.js";
 
 export const verifyToken = (req, res, next) => {
-  // Support both token from cookie and Authorization header
+  // Token from cookie or Authorization header
   const token =
-    req.cookies?.accessToken || // From cookie
-    (req.headers.authorization && req.headers.authorization.split(" ")[1]); // From Authorization: Bearer <token>
+    req.cookies?.accessToken ||
+    (req.headers.authorization && req.headers.authorization.split(" ")[1]);
 
-  if (!token) return next(createError(401, "You are not authenticated!"));
+  if (!token) {
+    return next(createError(401, "You are not authenticated!"));
+  }
 
   jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
-    if (err) return next(createError(403, "Token is not valid!"));
+    if (err) {
+      return next(createError(403, "Token is not valid!"));
+    }
 
-    // Attach user info to the request object
+    // Attach user info to request for use in route handlers
     req.userId = payload.id;
     req.isSeller = payload.isSeller;
     next();
